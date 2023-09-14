a08e60235b4b123eab79110e3ce8246fe0c48796
annaklimenko5248 <annaklimenko5248@gmail.com>
2023-09-14 19:26:43 +0300

    project_t1_consulting.iml

--- project_t1_consulting/src/main/java/T1_consulting/project_t1_consulting/FrequencyController.java
+++ project_t1_consulting/src/main/java/T1_consulting/project_t1_consulting/FrequencyController.java
@@ -4,27 +4,33 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class FrequencyController {
 
     @PostMapping("/frequency")
-    public Map<Character, Integer> calculateFrequency(@RequestBody Map<String, String> request) {
+    public Map<String, Integer> calculateFrequency(@RequestBody Map<String, String> request) {
         String inputString = request.get("inputString");
-        Map<Character, Integer> frequencyMap = new HashMap<>();
+        String[] arr = inputString.split("");
 
-        for (char c : inputString.toCharArray()) {
-            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
-        }
-        // Сортировка по убыванию количества вхождений
-        Map<Character, Integer> sortedMap = new LinkedHashMap<>();
-        frequencyMap.entrySet().stream()
-                .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
-                .forEachOrdered(entry -> sortedMap.put(entry.getKey(), entry.getValue()));
+        // Подсчет символов
+        Map<String, Integer> frequencyMap = Arrays.stream(arr)
+                .collect(Collectors.toMap(
+                        letter -> letter,
+                        letter -> 1,
+                        (v1, v2) -> v1 + v2
+                ));
 
-        return sortedMap;
+        return frequencyMap;
     }
 }
+
+
+
+
+
